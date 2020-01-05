fcf0ffdc18f85f013ea7e1dcb353e3e03ea14643
BOBsonic459 <bobsonic576@gmail.com>
2020-01-05 18:37:02 +0100

    msg

--- preload.js
+++ preload.js
@@ -6,7 +6,3 @@ window.addEventListener('DOMContentLoaded', () => {
     if (element) element.innerText = text
   }
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
-})
